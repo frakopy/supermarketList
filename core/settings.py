8279e4c1cae0e12923d643173e8d788c6f897170
frakopy <frako789@gmail.com>
2023-08-28 11:09:49 -0500

    adding origin and trusted

--- core/settings.py
+++ core/settings.py
@@ -133,3 +133,9 @@ MEDIA_ROOT = BASE_DIR / MEDIA_URL
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Origins and Trusted
+CORS_ALLOWED_ORIGINS = ["https://www.supermarketlist.fracodev.click"]
+
+CSRF_TRUSTED_ORIGINS = ["https://www.supermarketlist.fracodev.click"]
